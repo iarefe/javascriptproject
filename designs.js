eed66464589076e1ee0b831c4e89a0bcc3321020
FaisalGitHub <iarefe@hotmail.com>
2019-01-09 13:50:05 +0300

    the table code

--- designs.js
+++ designs.js
@@ -3,8 +3,26 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
+function makeGrid(event) {
+    event.preventdefault()
 // Your code goes here!
+var height = decument.GetElementById("inputheight").value;
+var width = decument.GetElementById("inputWidth").value;
+var TableCanvas = decument.GetElementById("pixelCanvas");
+
+TableCanvas.innerHTML="";
+
+for (var i=0; i<width;i++){
+    var row = decument.createElement("tr");
+    for (var j=0;j<width;j++){
+        var cell = decument.createElement("td");
+        row.appendchild(cell);
+    }
+    TableCanvas.appendchild(row);
+}
+}
+function colorCell(event) {
+
+
 
 }
